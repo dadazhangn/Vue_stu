6259c3ebf192ec82e647bae83f8df6a16f17ed11
dadazhang <2088437179@qq.com>
2025-03-19 21:54:23 +0800

    新增Element Plus组件，优化登录和首页布局，添加用户信息管理功能

--- shop-admin/src/main.js
+++ shop-admin/src/main.js
@@ -2,8 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Router from './tools/Router'
 import Store from './tools/Storage'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// import './assets/css/global.css'
 
 const app = createApp(App)
 app.use(Router)
 app.use(Store)
+app.use(ElementPlus)
 app.mount('#app')
--- shop-admin/src/tools/Storage.js
+++ shop-admin/src/tools/Storage.js
@@ -11,6 +11,16 @@ const Store = createStore ({
         isLogin: (state) => {
             return state.userName.length>0 && state.userPassword.length>0
         }
+    },
+    mutations: {
+        clearUserInfo(state) {
+            state.userName = ''
+            state.userPassword = ''
+        },
+        registUserInfo(state, {name, password}) {
+            state.userName = name
+            state.userPassword = password   
+        }
     }
 })
 
