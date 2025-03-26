c852eeb092bfe5f989844f5b3bf05014c72b229a
xoy <2088437179@qq.com>
2025-03-26 16:44:40 +0800

    添加mockjs依赖并实现订单数据模拟功能

---
+++ shop-admin/src/mock/Mock.js
@@ -0,0 +1,20 @@
+import mockjs from "mockjs";
+
+
+const mock = {
+    getOrder(type) {
+        let array = [];
+        for (let i = 0; i < mockjs.Random.integer(5,10); i++) {
+                array.push({mockjs.mock({
+                    'name': type=0 ? 'normal order' : 'vip order'+ i,
+                    'price': mockjs.Random.integer(20, 1000) + '$',
+                    'buyer': mockjs.Random.cname(),
+                    'time': mockjs.Random.datetime('yyyy-MM-dd HH:mm:ss'),
+                    'role': mockjs.Random.boolean,
+                    'state': mockjs.Random.boolean,
+                    'payType': mockjs.Random.boolean,
+                    'source':mockjs.Random.url(),
+                })})
+        }
+    }
+}
